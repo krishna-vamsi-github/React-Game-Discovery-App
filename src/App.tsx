0e2ad9dd5ca27b4922e8104ae3dc803e3b912c86
krishna-vamsi-github <anumala.krishnavamsi@gmail.com>
2023-11-28 09:13:41 +0530

    fixed isues in Navbar built perfect logo in Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Center, Grid, GridItem, Show } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 
 function App() {
@@ -10,25 +10,25 @@ function App() {
         lg: `"nav nav"
         "aside main"`,
       }}
-      gridTemplateRows={"50px 1fr"}
+      gridTemplateRows={"60px 1fr"}
       gridTemplateColumns={{
         base: "1fr",
-        lg: "150px 1fr"
+        lg: "150px 1fr",
       }}
       height="100vh"
       gap="1"
       color="blackAlpha.700"
       fontWeight="bold"
     >
-      <GridItem pl="4"  area={"nav"}>
+      <GridItem bg={"coral"} area={"nav"}>
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem pl="2" area={"aside"}>
+        <GridItem pl="2" bg={"lightgreen"} area={"aside"}>
           Aside
         </GridItem>
       </Show>
-      <GridItem pl="2"  area={"main"}>
+      <GridItem pl="2" bg={"lightpink"} area={"main"}>
         Main
       </GridItem>
     </Grid>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,34 +1,11 @@
-import {
-  HStack,
-  Input,
-  InputGroup,
-  InputLeftElement,
-  Switch,
-  useColorMode,
-  useColorModeValue,
-} from "@chakra-ui/react";
-import { GiAccordion } from "react-icons/gi";
-import { FaSearch } from "react-icons/fa";
+import { HStack, Image, Text } from "@chakra-ui/react";
+import logo from '../assets/logo.webp';
 
 const NavBar = () => {
-    const { colorMode, toggleColorMode } = useColorMode();
-    const bg = useColorModeValue('red.500', 'red.200')
-  const color = useColorModeValue('white', 'gray.800')
-  return (
-    <HStack spacing={10} align="center" bg={bg} color={color}>
-      <GiAccordion size={35} />
-
-      <InputGroup>
-        <InputLeftElement pointerEvents="none">
-          <FaSearch size={20} />
-        </InputLeftElement>
-        <Input placeholder="Search" />
-      </InputGroup>
-      <Switch size="md" onClick={toggleColorMode} width={40}>
-      Toggle {colorMode === 'light' ? 'dark' : 'light'}
-      </Switch>
-    </HStack>
-  );
+  return <HStack>
+    <Image src={logo} boxSize='60px'/>
+    <Text>NavBar</Text>
+  </HStack>;
 };
 
 export default NavBar;
