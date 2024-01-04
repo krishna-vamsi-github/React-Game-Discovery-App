325faa1ebf5ee83ec375e021a42d9b602a6a7945
krishna-vamsi-github <anumala.krishnavamsi@gmail.com>
2024-01-04 23:27:19 +0530

    Exercise: Creating Lookup Hooks

--- src/components/DynamicHeading.tsx
+++ src/components/DynamicHeading.tsx
@@ -3,17 +3,15 @@ import { GameQuery } from "../models/gameQuery.model";
 import useGames from "../hooks/useGames";
 import useGenres from "../hooks/useGenres";
 import usePlatforms from "../hooks/usePlatforms";
+import useGenre from "../hooks/useGenre";
+import usePlatform from "../hooks/usePlatform";
 
 interface Props {
   gameQuery: GameQuery;
 }
 const DynamicHeading = ({ gameQuery }: Props) => {
-  const game = useGenres().data?.results.find(
-    (genre) => genre.id === gameQuery.genreId
-  );
-  const platform = usePlatforms().data?.results.find(
-    (platform) => platform.id === gameQuery.genreId
-  );
+  const game = useGenre(gameQuery.genreId);
+  const platform = usePlatform(gameQuery.platformId);
   return (
     <>
       <Text fontWeight="bold" fontSize="xx-large">
